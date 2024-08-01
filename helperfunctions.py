4896404f2a92e33f1573852fd8cfbd5de8f18c9d
ElAleph2049 <yiheru@gmail.com>
2024-07-31 19:52:14 -0700

    added helper functions: computes the 2D cross product of two verctors, checks if one vec is on the front/back side relative to the other

---
+++ helperfunctions.py
@@ -0,0 +1,43 @@
+from glm import vec2
+
+
+def cross_2d(vec_0: vec2, vec_1: vec2):
+    '''
+    Computes the 2D cross product of two vectors.
+
+    Args:
+        vec_0 (vec2): The first vector.
+        vec_1 (vec2): The second vector.
+
+    Returns:
+        float: The 2D cross product of vec_0 and vec_1.
+    '''
+    return vec_0.x * vec_1.y - vec_1.x * vec_0.y
+
+
+def is_on_front(vec_0: vec2, vec_1: vec2):
+    '''
+    Determines if vec_0 is on the front side relative to vec_1.
+
+    Args:
+        vec_0 (vec2): The first vector.
+        vec_1 (vec2): The second vector.
+
+    Returns:
+        True if vec_0 is on the front side relative to vec_1, False otherwise
+    '''
+    return vec_0.x * vec_1.y < vec_1.x * vec_0.y
+
+
+def is_on_back(vec_0: vec2, vec_1: vec2):
+    '''
+    Determines if vec_0 is on the back side relative to vec_1.
+
+    Args:
+        vec_0 (vec2): The first vector.
+        vec_1 (vec2): The second vector.
+
+    Returns:
+        True if vec_0 is on the back side relative to vec_1, False otherwise
+    '''
+    return not is_on_front(vec_0, vec_1)
