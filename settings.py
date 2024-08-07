2d5bb74aaaea1a085feb9a5a9048f838cbdf8f59
ElAleph2049 <yiheru@gmail.com>
2024-08-07 00:59:57 -0700

    update settings

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
-import pyray as ray
-import glm
-from glm import vec2
+WIN_RES = WIN_WIDTH, WIN_HEIGHT = 1600, 900
 
+MAP_OFFSET = 50
+MAP_WIDTH, MAP_HEIGHT = WIN_WIDTH - MAP_OFFSET, WIN_HEIGHT - MAP_OFFSET
 
 EPS = 1e-4
